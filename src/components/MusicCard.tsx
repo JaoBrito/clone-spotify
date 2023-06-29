3adbf7513f9b76acbe518792f1723f40b285eb8b
Lucas Coutinho <lucas.coutinho@eagronom.com>
2023-06-28 22:38:31 -0300

    Improve

---
+++ src/components/MusicCard.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function MusicCard({
+  imageUrl,
+  title,
+  artist,
+  key,
+}: {
+  imageUrl: string;
+  title: string;
+  artist: string;
+  key: number;
+}) {
+  return (
+    <a
+      href=""
+      className="bg-white/5 p-2 rounded-lg flex flex-col gap-2 hover:bg-white/10"
+    >
+      <Image
+        src={imageUrl}
+        className="w-full"
+        width={120}
+        height={120}
+        alt="Capa da playist do veigh"
+      />
+      <strong className="font-semibold">{title}</strong>
+      <span className="text-sm text-zinc-400">{artist}</span>
+    </a>
+  );
+}
